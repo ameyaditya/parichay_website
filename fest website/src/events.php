7a93a1c3ea12423c6be3afd8c6d1e611af87c100
ameyaditya <ameyaditya.j@gmail.com>
2019-02-11 00:29:11 +0530

    added button increment and decrement facility

---
+++ fest website/src/events.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Parichay- Events</title>
+	<title>Parichay</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('.dec').click(function(){
+				var id = this.id;
+				id = id.split("-")[1];
+				counterid = "counter"+id;
+				var val = document.getElementById('counter-'+id).innerHTML;
+				val = Number(val);
+				if(val != 0){
+					val = val - 1;
+				}
+				$("#counter-"+id).text(String(val));
+			})
+			$('.inc').click(function(){
+				var id = this.id;
+				id = id.split("-")[1];
+				counterid = "counter"+id;
+				var val = document.getElementById('counter-'+id).innerHTML;
+				val = Number(val) + 1;
+				$("#counter-"+id).text(String(val));
+			})
+		});
+	</script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		  <a class="navbar-brand" href="#">Parichay</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav mr-auto">
+
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		      </li>
+
+		      <li class="nav-item dropdown active">
+		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		          Events
+		        </a>
+		        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		          <a class="dropdown-item" href="#">Technical</a>
+		          <a class="dropdown-item" href="#">Cultural</a>
+		          <a class="dropdown-item" href="#">Gaming</a>
+		          <a class="dropdown-item" href="#">Sports</a>
+		        </div>
+		      </li>
+
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Sponsors</a>
+		      </li>
+
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">About</a>
+		      </li>
+
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Contact Us</a>
+		      </li>
+
+		      <li class="nav-item">
+		      </li>
+		      
+		    </ul>
+		    	<p>
+		    		<a href="#">
+		          		<i class="fa fa-shopping-cart" style="font-size:24px; text-decoration: none; color: grey; text-decoration-color: grey;"></i>
+		          		 Cart
+		      		</a>
+		      	</p>
+		  </div>
+	</nav>
+	<h1 class="h1 eve-heading">Parichay - Events</h1>
+	<h1 class="h1 eve-heading-small">Parichay - Events</h1>
+	<div id="myBtnContainer">
+		<p class="lead">Filters</p>
+		<button class="btn active" onclick="filterSelection('all')"> Show all</button>
+		<button class="btn" onclick="filterSelection('technical')"> technical</button>
+		<button class="btn" onclick="filterSelection('cultural')"> Cultural</button>
+		<button class="btn" onclick="filterSelection('gaming')"> Gaming</button>
+		<button class="btn" onclick="filterSelection('sports')"> Sports</button>
+	</div>
+
+	<div class="event-box">
+		<div class="event-box-images">
+			
+		</div>
+		<div class="event-box-heading">
+			<h6 class="event-box-head">Heading</h6>
+		</div>
+		<div class="event-box-details">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco</p>
+		</div>
+		<div class="event-box-footer">
+			<button id="dec-1" class="dec">-</button><div class="counter" id="counter-1">0</div><button id="inc-1" class="inc">+</button>
+		</div>
+	</div>
+</body>
+</html>
---
+++ fest website/src/index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Parichay</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		  <a class="navbar-brand" href="#">Parichay</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav mr-auto">
+
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		      </li>
+
+		      <li class="nav-item dropdown">
+		        <a class="nav-link dropdown-toggle" href="events.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		          Events
+		        </a>
+		        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		          <a class="dropdown-item" href="events.php">Technical</a>
+		          <a class="dropdown-item" href="events.php">Cultural</a>
+		          <a class="dropdown-item" href="events.php">Gaming</a>
+		          <a class="dropdown-item" href="events.php">Sports</a>
+		        </div>
+		      </li>
+
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Sponsors</a>
+		      </li>
+
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">About</a>
+		      </li>
+
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Contact Us</a>
+		      </li>
+
+		      <li class="nav-item">
+		      </li>
+		      
+		    </ul>
+		    	<p>
+		    		<a href="#">
+		          		<i class="fa fa-shopping-cart" style="font-size:24px; text-decoration: none; color: grey; text-decoration-color: grey;"></i>
+		          		 Cart
+		      		</a>
+		      	</p>
+		  </div>
+	</nav>
+	<div id="carouselExampleIndicators" class="carousel slide cari" data-ride="carousel">
+	  <ol class="carousel-indicators">
+	    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+	  </ol>
+	  <div class="carousel-inner">
+	    <div class="carousel-item active">
+	      <img class="d-block w-100" src="images/1.jpg" alt="First slide">
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100" src="images/2.jpg" alt="Second slide">
+	    </div>
+	  </div>
+	  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+
+	<div class="container-fluid">
+		<h3 class="h3 ">About Parichay</h3>
+		<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+	<div class="container-fluid">
+		<h4 class="h2">Events</h4>
+		<div class="row">
+			<article class="col-md-3 eve">
+				<div class=event-images><img src="images/sports.png"></div>
+				<div class="cont">
+					<p>SPORTS</p>
+				</div>
+			</article>
+			<article class="col-md-3 eve">
+				<div class=event-images><img src="images/cultural.png"></div>
+				<div class="cont">
+					<p>CULTURAL</p>
+				</div>
+			</article>
+			<article class="col-md-3 eve">
+				<div class=event-images><img src="images/tech.png"></div>
+				<div class="cont">
+					<p>TECHNICAL</p>
+				</div>
+			</article>
+			<article class="col-md-3 eve">
+				<div class=event-images><img src="images/game.png"></div>
+				<div class="cont">
+					<p>GAMING</p>
+				</div>
+			</article>
+		</div>
+	</div>
+</body>
+</html>
