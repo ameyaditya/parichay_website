33bda33ed7d6631d6a09b54f3a19c39584467fb9
unknown <abhishekguptaatweb17@gmail.com>
2019-02-11 22:37:48 +0530

    add

--- fest website/src/events.php
+++ fest website/src/events.php
@@ -7,7 +7,12 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
+	<script
+			  src="https://code.jquery.com/jquery-3.3.1.min.js"
+			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+			  crossorigin="anonymous"></script>
+
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<script type="text/javascript">
@@ -72,8 +77,40 @@
 				$("#counter-"+id).text(String(val));
 				$("#amount-"+id).text("Rs."+String(updatedamount));
 			})
+			
+  // $("#cart-button").click(function(){
+  //   $.post("Instamojo.php",
+  //   {
+  //     finalAmount: updatedamount,
+  //   },
+  //   function(){
+  //     alert("Data: " + finalAmount );
+  //   });
+  // });
+
+
+			$(document).ready(function(){
+				$.ajax({
+			       url:"Instamojo.php",   // script location
+			       method:"POST",           
+			      
+			       data:{
+			 
+			          price:updatedamount,
+			        
+			        },
+			        success:function(data) 
+			        {
+			         $('#cart-button').html(data);        
+			         alert("Product has been added to cart ");  // even if no data is returned 
+			                                                   //alert will be shown
+			        }
+			    });
+			return false;
+					});
 
 			$("#cart-button").click(function(){
+		
 				final_content = "<div class='container-fluid'>";
 				if(cart_items.length == 0){
 					$("#cart-data").text("Cart Empty");
@@ -96,7 +133,8 @@
 				$("#cart-data").text("");
 				$("#total").text("");
 			})
-		});
+			});
+
 	</script>
 </head>
 <body>
